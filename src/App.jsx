78b6f4d9a7118c5d924f37b47b9a12036c57bf5c
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-05-05 17:05:23 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import styled from 'styled-components'
+import Hero from "./components/Hero"
+import Contact from "./components/Contact"
+import Works  from "./components/Works"
+import Who from "./components/Who"
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  color: white;
+  background: url("./img/bg.jpeg");
+  overflow-y: auto;
+  scrollbar-width: none;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
 function App() {
 
     return (
-      <div>
-        Hello world!
-      </div>
+      <Container>
+        <Hero />
+        <Works />
+        <Who />
+        <Contact />
+      </Container>
     )
   }
   
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+    display: flex;
+    
+`;
+
+const Navbar = () => {
+  return (
+    <Section>Navbar</Section>
+  )
+}
+
+export default Navbar
